211f11e2694c8f80656b34a28332b1b1ecefd4f8
Daniela Omena <rdaniglad@hotmail.com>
2019-05-27 20:52:18 -0400

    1 commit

---
+++ Chapter07ProgramDoWhileRandomNumbers/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Chapter07ProgramDoWhileRandomNumbers
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(39, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generate numbers between 0 to :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAnswer.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblAnswer.Location = new System.Drawing.Point(42, 151);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(300, 20);
+            this.lblAnswer.TabIndex = 1;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(246, 25);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(246, 98);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(42, 98);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 244);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Random Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Chapter07ProgramDoWhileRandomNumbers/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter07ProgramDoWhileRandomNumbers
+{
+    public partial class frmMain : Form
+    {
+
+        const int MAXITERATIONS = 200000; // Limit loop passes
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int counter; // Pass counter
+            int max; // Max value for random number
+            int last;
+            int current;
+            Random randomNumber = new Random();
+            //========= Program Input Step ================
+            flag = int.TryParse(txtMax.Text, out max);
+            if (flag == false)
+            {
+                MessageBox.Show("Digit characters only.", "Input Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                txtMax.Focus();
+                return;
+            }
+            //======== Program Process Step ==============
+            counter = 0;
+            last = (int)randomNumber.Next(max);
+            do
+            {
+                current = randomNumber.Next(max);
+                if (last == current)
+                {
+                    break;
+                }
+                last = current;
+                counter++;
+            } while (counter < MAXITERATIONS);
+            //========= Program Output Step ==============
+            if (counter < MAXITERATIONS)
+            {
+                lblAnswer.Text = "It took " + counter.ToString() + " passes to  match";
+            }
+            else
+            {
+                lblAnswer.Text = "No back-to-back match";
+            }
+
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+
+
+}
+
+
+    
+
